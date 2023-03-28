bd9bef16596c44a91a74c9ac8890e2ca6aea383d
Kamyar mazloom <77264406+kamyar-mazloom@users.noreply.github.com>
2023-03-28 17:47:29 +0330

    commit

---
+++ Chapter 2/Dog.java
@@ -0,0 +1,18 @@
+class Dog {
+  int size;
+  String breed;
+  String name;
+
+  void bark() {
+    System.out.println("ruff! ruff!");
+  }
+}
+
+class DogTestDrive {
+  public static void main(String[] args) {
+    Dog d = new Dog();
+    d.size = 40;
+    d.bark();
+
+  }
+}
